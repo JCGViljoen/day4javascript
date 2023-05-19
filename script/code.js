d533388767ad755b7e29b8bc3da67e5109ccf15c
JCGViljoen <jcgviljone51@gmail.com>
2023-05-19 09:54:23 +0200

    exerciseday4

---
+++ script/code.js
@@ -0,0 +1,15 @@
+let person1 = {
+    name: 'Jason',
+    age: 24
+    
+}
+ let person2 = {
+    name: 'Justin',
+    age: 25
+ }
+console.log(person1);
+console.log(person2);
+
+let array = [person1, person2];
+
+console.log(array)
